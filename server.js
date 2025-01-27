35407f779841a5a1498ccc5b1d6527e9dd4a7019
Karolis44 <karolis.jursenas@gmail.com>
2025-01-27 14:36:33 +0200

    invc

--- server.js
+++ server.js
@@ -198,6 +198,76 @@ app.get('/', (req, res) => {
     res.send(html);
 });
 
+/// Invoice 
+
+
+// fetch('https://in3.dev/inv/')
+//     .then(response => response.json())
+//     .then(data => {
+//         const invoiceData = data;
+
+
+//         document.getElementById('invoice-number').textContent = data.number;
+//         document.getElementById('invoice-date').textContent = data.date;
+//         document.getElementById('invoice-due-date').textContent = data.due_date;
+
+
+//         document.getElementById('seller-name').textContent = data.company.seller.name;
+//         document.getElementById('seller-address').textContent = data.company.seller.address;
+//         document.getElementById('seller-code').textContent = data.company.seller.code;
+//         document.getElementById('seller-vat').textContent = data.company.seller.vat;
+//         document.getElementById('seller-phone').textContent = data.company.seller.phone;
+//         document.getElementById('seller-email').textContent = data.company.seller.email;
+
+
+//         document.getElementById('buyer-name').textContent = data.company.buyer.name;
+//         document.getElementById('buyer-address').textContent = data.company.buyer.address;
+//         document.getElementById('buyer-code').textContent = data.company.buyer.code;
+//         document.getElementById('buyer-vat').textContent = data.company.buyer.vat;
+//         document.getElementById('buyer-phone').textContent = data.company.buyer.phone;
+//         document.getElementById('buyer-email').textContent = data.company.buyer.email;
+
+
+//         const tbody = document.querySelector('table tbody');
+//         let subtotal = 0;
+
+//         data.items.forEach(item => {
+//             const discountedPrice = calculateDiscountedPrice(item);
+//             subtotal += discountedPrice;
+
+//             const row = document.createElement('tr');
+//             row.innerHTML = `
+//                         <td>${item.description}</td>
+//                         <td>${item.quantity}</td>
+//                         <td>${item.price.toFixed(2)} €</td>
+//                         <td>${item.discount && item.discount.value ? (item.discount.type === 'fixed'
+//                     ? `-${(item.discount.value * item.quantity).toFixed(2)} €`
+//                     : `-${((item.price * item.discount.value / 100) * item.quantity).toFixed(2)} € (-${item.discount.value}%)`) : '-'}</td>
+//                         <td>${discountedPrice.toFixed(2)} €</td> `;
+//             tbody.appendChild(row);
+//         });
+
+
+//         const vat = subtotal * 0.21;
+//         const total = subtotal + vat + data.shippingPrice;
+
+//         document.getElementById('shipping-price').textContent = `${data.shippingPrice.toFixed(2)} €`;
+//         document.getElementById('subtotal').textContent = `${subtotal.toFixed(2)} €`;
+//         document.getElementById('vat').textContent = `${vat.toFixed(2)} €`;
+//         document.getElementById('total').textContent = `${total.toFixed(2)} €`;
+//     })
+
+
+// function calculateDiscountedPrice(item) {
+//     if (item.discount && item.discount.type === "fixed") {
+//         return (item.price - item.discount.value) * item.quantity;
+//     } else if (item.discount && item.discount.type === "percentage") {
+//         const discount = item.price * (item.discount.value / 100);
+//         return (item.price - discount) * item.quantity;
+//     }
+//     return item.price * item.quantity;
+// }
+
 
 
 
